482bf4b3a96b1d615637ec1e105fc8dffdbea4ba
Desha-Saeed <deshasaeed77@gmail.com>
2023-05-10 14:22:12 +0300

    added validation on  routes

--- models/student.js
+++ models/student.js
@@ -16,14 +16,7 @@ const StudentSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      trim: true,
-      lowercase: true,
-      unique: [true, 'email must be unique'],
       required: 'email address is required',
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please fill a valid email address',
-      ],
     },
 
     password: {
